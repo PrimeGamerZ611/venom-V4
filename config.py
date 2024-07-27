d09750e3eb7237388caa1dcb486bd2ed33bcd0fe
PrimeGamerZ611 <pratikdeshmukh912@gmail.com>
2024-07-27 16:20:50 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 #SCRIPT BY VENOMxCRAZY
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("7424479555:AAG21En_vWhghzaUAhKjJ-DoZaLV3KBH4")
+BOT_TOKEN=("7344489561:AAFbDr2RZCcnTdlQlSX-xN3W6X25l-LACLY")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("venomXcrazy")
+OWNER_USERNAME=("Prime_GamerZ")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("5588464519")
+ADMIN_IDS=("5188936165")
 
 
 
